9ba46723183752975315bc261802f4d630ded009
hafiz-panda-software <80079662+hafiz-panda-software@users.noreply.github.com>
2023-02-22 10:59:46 +0800

    Added flutter_html package

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:panda_flutter_ui/panda_flutter_ui.dart';
+import 'package:panda_flutter_ui/panda_widgets/html/panda_html_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,6 +57,36 @@ class _MyHomePageState extends State<MyHomePage> {
                 isZoomableImage: true,
                 isGreyImage: true,
               ),
+            ),
+            PandaHtmlView(
+              data: '''
+  <H1>Heading 1</H1>
+  <H2>Heading 2</H2>
+  <H3>Heading 3</H3>
+  <H4>Heading 4</H4>
+  <H5>Heading 5</H5>
+  <H6>Heading 6</H6>
+  <p>Lorem ipsum dolor sit amet, consectetur <a href="https://google.com/">Google!</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+  <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkZfSUuIOGln-FOAW3OSoJquZXRolTjZhseQ&usqp=CAU"/>
+    <br/>
+    <table>
+    <thead>
+    <tr><th>One</th><th>Two</th><th>Three</th></tr>
+    </thead>
+    <tbody>
+    </tbody>
+    <tfoot>
+    <tr><td>Data</td><td>Data</td><td>Data</td></tr>
+    <tr><td>Data</td><td>Data</td><td>Data</td></tr>
+    <tr><td>Data</td><td>Data</td><td>Data</td></tr>
+    </tfoot>
+    </table>''',
+              accentColor: Colors.orange[900],
+              textColor: Colors.grey[700],
+              onLinkTap: (url) {
+                print('html url: $url');
+              },
             ),
           ],
         ),
