33640528e33531be95da8ff7ebee0d5f0612479e
hafiz-panda-software <80079662+hafiz-panda-software@users.noreply.github.com>
2024-03-27 14:26:25 +0800

    Version 0.0.17

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,20 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import just_audio
 import path_provider_foundation
 import sqflite
+import url_launcher_macos
+import video_player_avfoundation
+import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
+  WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }
--- lib/panda_widgets/html/panda_html_view.dart
+++ lib/panda_widgets/html/panda_html_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 // import 'package:flutter_html/flutter_html.dart';
 import 'package:panda_flutter_ui/panda_flutter_ui.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
 class PandaHtmlView extends StatelessWidget {
   final String data;
@@ -34,114 +35,117 @@ class PandaHtmlView extends StatelessWidget {
   Widget build(BuildContext context) {
     final PandaTextFormatService _textFormatService = PandaTextFormatService();
     // return Container();
-    return Html(
-      data: _textFormatService.fixEmoji(
+    return HtmlWidget(
+      _textFormatService.fixEmoji(
         _textFormatService.cleanAmpersand(
           _textFormatService.sanitizeHtml(data),
         ),
       ),
-      onLinkTap: (url, context, a) {
+      onTapUrl: (url) {
         if (onLinkTap != null) {
           onLinkTap!(url);
+          return true;
+        } else {
+          return false;
         }
       },
-      style: {
-        "*": Style(
-          textAlign: textAlign,
-          margin: Margins.zero,
-        ),
-        "h1": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(30),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "h2": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(25),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "h3": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(22),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "h4": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(20),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "h5": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(18),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "h6": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(15),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: FontWeight.bold,
-        ),
-        "p": Style(
-          margin: Margins.only(bottom: 15),
-          textDecorationColor: textColor,
-          fontSize: FontSize(fontSize ?? 15),
-          color: textColor,
-          textAlign: textAlign,
-          fontWeight: fontWeight,
-        ),
-        "a": Style(
-          margin: Margins.all(0),
-          textDecorationColor: textColor,
-          fontSize: FontSize(fontSize ?? 15),
-          color: accentColor,
-          textAlign: textAlign,
-          fontWeight: fontWeight,
-        ),
-        "table": Style(
-            // backgroundColor: const Color.fromARGB(0x50, 0xee, 0xee, 0xee),
-            ),
-        // some other granular customizations are also possible
-        "tr": Style(
-          border: Border(
-            bottom: BorderSide(
-              color: borderColor ?? Colors.grey,
-            ),
-          ),
-        ),
-        "th": Style(
-          padding: HtmlPaddings.all(10),
-          backgroundColor: Colors.grey,
-        ),
-        "td": Style(
-          padding: HtmlPaddings.all(10),
-          alignment: Alignment.topLeft,
-        ),
-        "body": isRemoveBodyPadding
-            ? Style(
-                // margin: EdgeInsets.zero,
-                margin: Margins.zero,
-                // padding: EdgeInsets.zero,
-                padding: HtmlPaddings.zero,
-              )
-            : Style()
-      },
+      // style: {
+      //   "*": Style(
+      //     textAlign: textAlign,
+      //     margin: Margins.zero,
+      //   ),
+      //   "h1": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(30),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "h2": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(25),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "h3": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(22),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "h4": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(20),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "h5": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(18),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "h6": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(15),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: FontWeight.bold,
+      //   ),
+      //   "p": Style(
+      //     margin: Margins.only(bottom: 15),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(fontSize ?? 15),
+      //     color: textColor,
+      //     textAlign: textAlign,
+      //     fontWeight: fontWeight,
+      //   ),
+      //   "a": Style(
+      //     margin: Margins.all(0),
+      //     textDecorationColor: textColor,
+      //     fontSize: FontSize(fontSize ?? 15),
+      //     color: accentColor,
+      //     textAlign: textAlign,
+      //     fontWeight: fontWeight,
+      //   ),
+      //   "table": Style(
+      //       // backgroundColor: const Color.fromARGB(0x50, 0xee, 0xee, 0xee),
+      //       ),
+      //   // some other granular customizations are also possible
+      //   "tr": Style(
+      //     border: Border(
+      //       bottom: BorderSide(
+      //         color: borderColor ?? Colors.grey,
+      //       ),
+      //     ),
+      //   ),
+      //   "th": Style(
+      //     padding: HtmlPaddings.all(10),
+      //     backgroundColor: Colors.grey,
+      //   ),
+      //   "td": Style(
+      //     padding: HtmlPaddings.all(10),
+      //     alignment: Alignment.topLeft,
+      //   ),
+      //   "body": isRemoveBodyPadding
+      //       ? Style(
+      //           // margin: EdgeInsets.zero,
+      //           margin: Margins.zero,
+      //           // padding: EdgeInsets.zero,
+      //           padding: HtmlPaddings.zero,
+      //         )
+      //       : Style()
+      // },
     );
   }
 }
